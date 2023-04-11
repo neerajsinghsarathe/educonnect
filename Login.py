5d5064253bf010ae45a1c33bde15ac3d76b5bcd5
Neeraj Singh Sarathe <neerajssarathe@gmail.com>
2023-04-12 02:42:49 +0530

    added code in homepage.py for reference -> Completed functionality for main and login including navigation

---
+++ Login.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+from tkinter import messagebox
+
+
+def login(email, password, usertype):
+    if usertype == 'student':
+        if email == 'student' and password == '123456':
+            messagebox.showinfo(title="Login Status", message="success")
+    if usertype == 'lecturer':
+        if email == 'lecturer' and password == '5678':
+            messagebox.showinfo(title="Login Status", message="success")
+
+
+class Login(tk.Tk):
+    def __init__(self, usertype):
+        super().__init__()
+
+        self.geometry("500x300")
+        self.title(f'{usertype} login')
+        self.bind("<KeyPress>", self.shortcut)
+        label = tk.Label(self, text="Enter your Credentials", font=('Helvetica', 18))
+        label.pack(padx=10, pady=20)
+
+        self.email = tk.StringVar()
+        self.password = tk.StringVar()
+        self.userType = usertype
+
+        frame = tk.Frame(self)
+        frame.columnconfigure(0, weight=1)
+        frame.columnconfigure(1, weight=1)
+
+        emailLabel = tk.Label(frame, text="Login ID", font=('Helvetica', 14))
+        emailLabel.grid(row=0, column=0, padx=10, pady=10)
+
+        emailBox = tk.Entry(frame, width=15, font=('Arial', 14), textvariable=self.email)
+        emailBox.grid(row=0, column=1, padx=10, pady=10)
+
+        passwordLabel = tk.Label(frame, text="Password", font=('Helvetica', 14))
+        passwordLabel.grid(row=1, column=0, padx=10, pady=10)
+
+        passwordBox = tk.Entry(frame, width=15, show='*', font=('Arial', 14), textvariable=self.password)
+        passwordBox.grid(row=1, column=1, padx=10, pady=10)
+        frame.pack(fill='x', pady=10, padx=10)
+
+        loginButton = tk.Button(self, text="Login", font=('Helvetica', 14), command=self.login)
+        loginButton.pack(padx=10, pady=10)
+
+        exitButton = tk.Button(self, text="Exit", font=('Helvetica', 14), command=self.destroy)
+        exitButton.pack(padx=10, pady=10)
+
+    def login(self):
+        email = self.email.get()
+        password = self.password.get()
+        login(email, password, self.userType)
+
+    def shortcut(self, event):
+        if event.keysym == 'Return':
+            return self.login()
--- main.py
+++ main.py
@@ -1,55 +1,35 @@
 import tkinter as tk
-import tkinter.messagebox
+import subprocess as sb_p
+from Login import Login
 
 
-class HomePage(tk.Tk):
+def navigateTo(current, userType):
+    current.destroy()
+    Login(userType).mainloop()
+
+
+class Main(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        self.geometry("500x300")
+        self.geometry("400x400")
         self.title("EduConnect")
-        self.bind("<KeyPress>", self.shortcut)
         label = tk.Label(self, text="Enter your Credentials", font=('Helvetica', 18))
         label.pack(padx=10, pady=20)
 
-        self.email = tk.StringVar()
-        self.password = tk.StringVar()
+        studentLoginButton = tk.Button(self, width=20, text="Student Login", font=('Helvetica', 14),
+                                       command=lambda: navigateTo(self, 'student'))
+        studentLoginButton.pack(padx=10, pady=10)
 
-        frame = tk.Frame(self)
-        frame.columnconfigure(0, weight=1)
-        frame.columnconfigure(1, weight=1)
+        lecturerLoginButton = tk.Button(self, width=20, text="Lecturer Login", font=('Helvetica', 14),
+                                        command=lambda: navigateTo(self, 'lecturer'))
+        lecturerLoginButton.pack(padx=10, pady=10)
 
-        emailLabel = tk.Label(frame, text="Login ID", font=('Helvetica', 14))
-        emailLabel.grid(row=0, column=0, padx=10, pady=10)
-
-        emailBox = tk.Entry(frame, width=15, font=('Arial', 14), textvariable=self.email)
-        emailBox.grid(row=0, column=1, padx=10, pady=10)
-
-        passwordLabel = tk.Label(frame, text="Password", font=('Helvetica', 14))
-        passwordLabel.grid(row=1, column=0, padx=10, pady=10)
-
-        passwordBox = tk.Entry(frame, width=15, show='*', font=('Arial', 14), textvariable=self.password)
-        passwordBox.grid(row=1, column=1, padx=10, pady=10)
-        frame.pack(fill='x', pady=10, padx=10)
-
-        loginButton = tk.Button(self, text="Login", font=('Helvetica', 14), command=self.login)
-        loginButton.pack(padx=10, pady=10)
-
-        exitButton = tk.Button(self, text="Exit", font=('Helvetica', 14), command=self.destroy)
-        exitButton.pack(padx=10, pady=10)
-
-    def login(self):
-        email = self.email.get()
-        password = self.password.get()
-
-        if email == 'student' and password == 'cse':
-            tkinter.messagebox.showinfo(title="Login Status", message="success")
-
-    def shortcut(self, event):
-        if event.keysym == 'Return':
-            return self.login()
+        newWindowButton = tk.Button(self, width=20, text="New Window", font=('Helvetica', 14),
+                                    command=lambda: sb_p.call("start python main.py", shell=True))
+        newWindowButton.pack(padx=10, pady=10)
 
 
 if __name__ == "__main__":
-    app = HomePage()
+    app = Main()
     app.mainloop()
